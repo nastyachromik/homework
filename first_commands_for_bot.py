f5d3b7dc3978d017dc6f064f44ce47bc356483af
Nastya Khromenkova Т <nastychromik@gmail.com>
2024-07-24 12:02:24 +0300

    weather function for bot

--- first_commands_for_bot.py
+++ first_commands_for_bot.py
@@ -1,12 +1,14 @@
 import requests
 import time
-import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 API_URL = 'https://api.telegram.org/bot'
 token = '6870654449:AAH5VSfXmALYnVDbFWayDNux6iP6QhbXk8E'
 MAX_COUNTER = 100
 counter = 0
 offset = -2
+
 
 while counter < MAX_COUNTER:
     response = requests.get(url=f'{API_URL}{token}/getUpdates?offset={offset + 1}').json()
@@ -15,6 +17,7 @@ while counter < MAX_COUNTER:
         req = response['result'][0]['message']['text']
         name = response['result'][0]['message']['from']['first_name']
         chat_id = response['result'][0]['message']['chat']['id']
+        offset = response['result']['update_id']
 
         if 'привет' in req.lower().strip():
             requests.get(url=f'{API_URL}{token}/sendMessage?chat_id={chat_id}&text=Привет, {name}!')
@@ -47,6 +50,24 @@ while counter < MAX_COUNTER:
             while True:
                 response = requests.get(url=f'{API_URL}{token}/getUpdates?offset={offset + 1}').json()
                 if response['result']:
+                    offset = response['result']['update_id']
                     city = response['result'][0]['message']['text']
                     break
-            gismeteo = requests.get(url='https://www.gismeteo.ru/')
+            driver = webdriver.Chrome()
+            driver.get(url='https://www.gismeteo.ru/')
+            button = driver.find_element(By.CLASS_NAME('input js-input'))
+            try:
+                button.send_keys(f'{city.strip()}')
+                weather = driver.find_element(By.CLASS_NAME, 'weather-value').text
+                requests.get(
+                    f'{API_URL}{token}/sendMessage?chat_id={chat_id}&text=Погода в городе {city} сейчас: {weather}')
+            except:
+                requests.get(
+                    f'{API_URL}{token}/sendMessage?chat_id={chat_id}&text=Вероятно, такого города не существует(')
+        else:
+            message = 'Вы ввели некорректную команду. Список доступных команд:\n1) Привет\n2) Пока\n3) Как дела?\n4)Мои координаты\n5) Погода'
+            requests.get(
+                f'{API_URL}{token}/sendMessage?chat_id={chat_id}&text={message}')
+
+    counter += 1
+    time.sleep(1)
